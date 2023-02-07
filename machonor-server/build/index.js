19aaa33703ee7544949d8483b532fa76149257c1
sangHyun <public.winverse@gmail.com>
2023-02-07 22:19:29 +0900

    Fix: Remove build

--- machonor-server/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv = require("dotenv");
-dotenv.config();
-const server_1 = require("server");
-const server = new server_1.default();
-server.listen(4000);
-//# sourceMappingURL=index.js.map
--- machonor-server/build/lib/sendMail.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const AWS = require("aws-sdk");
-const ses = new AWS.SES({});
-//# sourceMappingURL=sendMail.js.map
